85cec69c88782cae59c71b8cba67e45d6a925de2
Shipeng Xu <sxu@Shipeng-Xu.local>
2016-04-21 15:54:59 -0700

    added new files.

---
+++ src/main/java/com/shipeng/xml/App.java
@@ -0,0 +1,13 @@
+package com.shipeng.xml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
